b3b6b5ba16ef9969fa0527d4094a6862de4e70a3
Thanuja Mudalige <265001976+Thanuja-Mudalige@users.noreply.github.com>
2026-06-26 18:10:13 +0530

    add darkning with hover count effect

--- Js/index.js
+++ Js/index.js
@@ -13,8 +13,20 @@ makegrid();
 
 let divs=document.querySelectorAll('.main div');
 divs.forEach(
-    (div)=>{div.addEventListener('mouseenter',()=>{
-            div.classList.add('hover')
+    (div)=>{div.addEventListener('mouseenter',(e)=>{
+        let count = Number(e.target.dataset.hits || 0);
+        count++;
+        e.target.dataset.hits = count;
+        if(count==1){
+            div.classList.add('hover');}
+        else if(count==2){
+            div.classList.remove('hover');
+            div.classList.add('hoverleveltwo');
+        }
+        else{
+            div.classList.remove('hover','hoverleveltwo');
+            div.classList.add('hoverlevelthree');
+        }
 }
 )
 }
