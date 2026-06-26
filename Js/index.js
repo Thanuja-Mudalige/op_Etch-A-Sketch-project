caa9fe6d698231045332bbf23f244c4693bed7c4
Thanuja Mudalige <265001976+Thanuja-Mudalige@users.noreply.github.com>
2026-06-26 18:44:40 +0530

    add change canvas size button and logic

--- Js/index.js
+++ Js/index.js
@@ -1,16 +1,20 @@
 let maincontainer=document.querySelector('.main');
 
-function makegrid(){
-for(let i=0;i<16;i++){
-    for(let j=0;j<16;j++){
+function makegrid(size=16){
+maincontainer.replaceChildren();
+maincontainer.style.setProperty('--size', size); 
+for(let i=0;i<size;i++){
+    for(let j=0;j<size;j++){
         let divsingle = document.createElement("div");
         maincontainer.appendChild(divsingle);
          divsingle.textContent = `${i},${j}`;
     }
 }
+listnerattach();
 }
 makegrid();
 
+function listnerattach(){
 let divs=document.querySelectorAll('.main div');
 divs.forEach(
     (div)=>{div.addEventListener('mouseenter',(e)=>{
@@ -31,3 +35,15 @@ divs.forEach(
 )
 }
 )
+}
+
+document.querySelector('.changesize').addEventListener('click',()=>{
+    let canvassize=prompt('How many number of squares per side?');
+    if(canvassize>100){
+        alert('Enter a number less than or equal to 100');
+    }
+    else{
+    makegrid(canvassize);
+    }
+})
+
